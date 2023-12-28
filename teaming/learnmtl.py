8abb7c4fba57a60bb1813a2f5d6525c85b387f67
jaeioursh <joshc1010@gmail.com>
2023-12-28 07:39:23 -0800

    fc + align

--- teaming/learnmtl.py
+++ teaming/learnmtl.py
@@ -316,9 +316,9 @@ class learner:
                 G.append(g)
             
 
-        if train_flag==2 or train_flag==1 or train_flag==0 or train_flag>5:
+        if train_flag!=6 and (train_flag==2 or train_flag==1 or train_flag==0 or train_flag>5):
             self.updateA(train_flag)
-        if train_flag==3:
+        if train_flag==3 or train_flag==6:
             self.updateD(env)
         train_set=np.unique(np.array(self.team))
         for t in np.unique(np.array(self.team)):
@@ -334,7 +334,7 @@ class learner:
                 if  train_flag==5:
                     p.fitness=np.sum(p.G)
 
-                if train_flag==3:
+                if train_flag==3 or train_flag==6:
                     p.D=[self.Dapprox[t].feed(np.array(p.S[i])) for i in range(len(p.S))]
                     self.log.store("ctime",[np.argmax(i) for i in p.D])
                     p.D=[np.max(i) for i in p.D]
@@ -342,7 +342,7 @@ class learner:
                     #print(p.D)
                     p.fitness=np.sum(p.D)
                     
-                if train_flag==1 or train_flag==0 or train_flag>5 or train_flag==2:
+                if train_flag!=6 and (train_flag==1 or train_flag==0 or train_flag>5 or train_flag==2):
                     if train_flag==1 or train_flag==7 or train_flag==0 or train_flag==6  or train_flag==2:
                         p.D=list(self.align[t].feed(np.array(p.Z)))
                     else:
